3657aa354f857187ebf09e4234e1ebb1b5ab0e32
james <jjlong@mit.edu>
2017-05-02 21:27:27 -0400

    added choice of frequency

--- application.py
+++ application.py
@@ -17,6 +17,7 @@ import io
 import csv
 from sqlalchemy import func
 import itertools
+import functools
 # Elastic Beanstalk initalization
 
 from application import application
@@ -35,8 +36,7 @@ def experiments():
     return Response(json.dumps(all_experiments), status = 200)
 
 def extract_meta(row):
-    range_in_secs = int((row.max_sequence_id-row.min_sequence_id)/1000)
-    return {'name': row.folder_name, 'range':range_in_secs ,
+    return {'name': row.folder_name,
             'damage': row.damage, 'excitation': row.excitation,
             'minseq':row.min_sequence_id, 'maxseq':row.max_sequence_id}
 def just_data(row):
@@ -60,41 +60,59 @@ def int_to_g(accel):
     volt_to_g = 5/3.3
     num_to_g = num_to_volt*accel*volt_to_g
     return num_to_g
+def possible_nodes():
+    return [5, 6, 7, 15, 17, 18, 20, 21, 22, 23, 24, 25, 27, 29, 30, 31, 32, 33, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 52, 53, 54, 55, 56, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69]    
 @application.route('/large.csv', methods = ['POST'])
 def generate_large_csv():
     #start_seq = request.form.min_sequence
     #end_seq = request.form.max_sequence
+    print('req: ', dict(request.form))
+    specs = {k:v for k,v in request.form.items()}
+    print("specs: ", specs)
+    query = form_query(specs)
+    arrs = get_arrs(query)
+    noderange = possible_nodes()
+    writer = get_file_writer(arrs, noderange)
+    response = Response(writer(), mimetype='text/csv')
+    #response = Response(status = 200)
+    response.headers["Content-Disposition"] = "attachment; filename=test.csv"
+    return response
+def form_query(specs):
     jsonified = func.array_to_json(func.array_agg(func.row(accel_data.seq_id, accel_data.node, accel_data.axis, accel_data.accel)))
-    node_range = [5, 6, 7, 15, 17, 18, 20, 21, 22, 23, 24, 25, 27, 29, 30, 31, 32, 33, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 52, 53, 54, 55, 56, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69]
-    translation = {'f1':'seq_id', 'f2':'node', 'f3':'axis','f4':'accel'}
+    starting_seq = int(specs['min_sequence'])
+    ending_seq = min(int(specs['max_sequence']), starting_seq+1000*int(specs['user_length']))
+    skip_every = int(1000/int(specs['freq']))
+    print(starting_seq, ending_seq)
     vectors = (db.session.query(accel_data.seq_id, jsonified)
              .group_by(accel_data.seq_id)
              .order_by(accel_data.seq_id)
-             .filter(accel_data.seq_id.between(30124655140, 30124665140))
-             )
-    just_arrays = (just_data(i) for i in vectors)
+             .filter(accel_data.seq_id.between(starting_seq, ending_seq))
+             .filter(accel_data.seq_id%skip_every == 0))
+    return vectors
+def get_arrs(query):
+    translation = {'f1':'seq_id', 'f2':'node', 'f3':'axis','f4':'accel'}
+    just_arrays = (just_data(i) for i in query)
     translated = (arraydict_translator(translation, arr) for arr in just_arrays)
     tstamped_arrs = (merge_timestep(onet) for onet in translated)
-    headers = ["seq_id"]+[stringify(i) for i in itertools.product(node_range, ['x','y','z'])]
+    return tstamped_arrs
+def get_file_writer(arrs, noderange):
+    headers = ["seq_id"]+[stringify(i) for i in itertools.product(noderange, ['x','y','z'])]
     header_dict = collections.OrderedDict()
     for k in headers:
         header_dict[k] = None
-    def data():
+    def data_writer(header_dictionary,arrays):
         csvfile = io.StringIO()
-        headerwriter = csv.DictWriter(csvfile, delimiter=',', fieldnames = header_dict)
+        headerwriter = csv.DictWriter(csvfile, delimiter=',', fieldnames = header_dictionary)
         headerwriter.writeheader()
         yield csvfile.getvalue()
-        for i in tstamped_arrs:
-            test_dixt = collections.OrderedDict({k:i.get(k,None) for k in header_dict})
+        for i in arrays:
+            print('got an arr', i)
+            test_dixt = collections.OrderedDict({k:i.get(k,None) for k in header_dictionary})
             csvfile = io.StringIO()
-            csvwriter = csv.DictWriter(csvfile, delimiter=',', fieldnames = header_dict)
+            csvwriter = csv.DictWriter(csvfile, delimiter=',', fieldnames = header_dictionary)
             csvwriter.writerow(test_dixt)
             val = csvfile.getvalue()
             yield val
-    response = Response(data(), mimetype='text/csv')
-    response.headers["Content-Disposition"] = "attachment; filename=test.csv"
-    return response
-
-
+    return functools.partial(data_writer, header_dict, arrs)
 if __name__ == '__main__':
     application.run()
--- application/static/Main.elm
+++ application/static/Main.elm
@@ -1,12 +1,14 @@
--- Read more about this program in the official Elm guide:
 -- https://guide.elm-lang.org/architecture/effects/http.html
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes as H exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Decode exposing (Decoder, field, succeed, map2, list, string, bool, dict, int, maybe, decodeString, map)
 import Table
+import Array
+import Dict
+import Html.Events.Extra exposing (targetValueIntParse)
 
 main =
   Html.program
@@ -17,6 +19,13 @@ main =
   }
 
 -- MODEL
+type alias RawExperiment =
+  { name : String
+  , excitation: String
+  , damage: String
+  , minseq: Int
+  , maxseq: Int
+  }
 type alias Experiment = 
   { name : String
   , excitation: String
@@ -24,12 +33,24 @@ type alias Experiment =
   , minseq: Int
   , maxseq: Int
   , range: Int
+  , userlength: String
+  , userfreq:Int
+  }
+type alias FileLength = 
+  { file: String
+  , len: String
+  }
+type alias FileFreq = 
+  { file: String
+  , freq: Int
   }
 
 type alias Model =
   { experimentlist : List Experiment
   , tableState : Table.State
   , query : String
+  , lenParams: Dict.Dict String String
+  , freqParams:  Dict.Dict String Int
   }
 
 
@@ -40,6 +61,8 @@ init =
       { experimentlist = []
       , tableState = Table.initialSort "name"
       , query = ""
+      , lenParams = Dict.empty
+      , freqParams = Dict.empty
       }
   in
     ( model, fetchExperiments )
@@ -48,9 +71,26 @@ init =
 
 type Msg
   = Experiments
-  | FetchList (Result Http.Error (List Experiment))
+  | FetchList (Result Http.Error (List RawExperiment))
   | SetQuery String
   | SetTableState Table.State
+  | SelectSubrange FileLength
+  | SelectFreq FileFreq
+
+negate: Int -> Int -> Bool -> Bool
+negate chosen index element =
+    if chosen == index then not element else element
+
+negateArray: Int -> Array.Array Bool -> Array.Array Bool
+negateArray index arr = 
+  Array.indexedMap (negate index) arr
+
+unraw: RawExperiment -> Experiment
+unraw {name, excitation, damage, minseq, maxseq} =
+  let 
+    range = (maxseq-minseq)//1000
+    inituserlength = (toString range)
+  in Experiment name excitation damage minseq maxseq range inituserlength 1000
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -59,7 +99,7 @@ update msg model =
       (model, fetchExperiments)
 
     FetchList (Ok experiments) ->
-      ({ model | experimentlist = experiments }
+      ({ model | experimentlist = (List.map unraw experiments) }
       , Cmd.none
       )
 
@@ -75,6 +115,15 @@ update msg model =
       ({ model | tableState = newState }
       , Cmd.none
       )
+    SelectSubrange newrange ->
+      ({model|lenParams = (Dict.insert newrange.file newrange.len) model.lenParams}
+      , Cmd.none
+      )
+    SelectFreq newfreq ->
+      ({model|freqParams = (Dict.insert newfreq.file newfreq.freq) model.freqParams}
+      , Cmd.none
+      )
+
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
@@ -83,19 +132,41 @@ subscriptions model =
 
 -- VIEW
 view : Model -> Html Msg
-view { experimentlist, tableState, query } =
+view { experimentlist, tableState, query, lenParams, freqParams} =
   let
     lowerQuery =
       String.toLower query
-
+    withlengths = 
+      List.map (zipExperimentLengths lenParams) experimentlist
+    withlengthsfreqs = 
+      List.map (zipExperimentFreqs freqParams) withlengths
     acceptableExperiments =
-      List.filter (String.contains lowerQuery << String.toLower << .name) experimentlist
+      List.filter (String.contains lowerQuery << String.toLower << .name) withlengthsfreqs
   in
-    div []
+    div [class "container"]
       [ h1 [] [ text "Available experiments" ]
       , input [ placeholder "Search by Name", onInput SetQuery ] []
       , Table.view config tableState acceptableExperiments
       ]
+zipExperimentLengths: Dict.Dict String String -> Experiment -> Experiment
+zipExperimentLengths userdefs exp =
+  let lenInSecs = getExpLength exp.name userdefs
+  in {exp|userlength = lenInSecs}
+
+getExpLength: String -> Dict.Dict String String -> String
+getExpLength fname d =
+  Dict.get fname d
+      |> Maybe.withDefault "0"
+
+zipExperimentFreqs: Dict.Dict String Int-> Experiment -> Experiment
+zipExperimentFreqs userdefs exp =
+  let freq = getExpFreq exp.name userdefs
+  in {exp|userfreq = freq}
+
+getExpFreq: String -> Dict.Dict String Int -> Int
+getExpFreq fname d =
+  Dict.get fname d
+      |> Maybe.withDefault 100
 
 config : Table.Config Experiment Msg
 config =
@@ -107,41 +178,98 @@ config =
         , Table.intColumn "Length (s)" .range
         , Table.stringColumn "Damage" .damage
         , Table.stringColumn "Excitation" .excitation
+        , inputLength
+        , inputFreq
         , downloadColumn]
     }
 
 downloadColumn : Table.Column Experiment Msg
 downloadColumn =
   Table.veryCustomColumn
-    { name = ""
+    { name = "Download"
     , viewData = viewDownload
     , sorter = Table.unsortable
     }
+
 viewDownload : Experiment -> Table.HtmlDetails Msg
 viewDownload exp =
   Table.HtmlDetails []
     [Html.form [action "/large.csv", method "post"] 
                [input [type_ "hidden", name "folder_name", value exp.name] []
+               ,input [type_ "hidden", name "user_length", value exp.userlength] []
                ,input [type_ "hidden", name "min_sequence", value (toString exp.minseq)] []
                ,input [type_ "hidden", name "max_sequence", value (toString exp.maxseq)] []
-               , button [type_ "submit"] [text "Download csv"] ]]
+               ,input [type_ "hidden", name "freq", value (toString exp.userfreq)] []
+               ,button [type_ "submit"] [text "Download csv"] ]]
 
+inputFreq : Table.Column Experiment Msg
+inputFreq =
+  Table.veryCustomColumn
+    { name = "Desired frequency (Hz)"
+    , viewData = viewFreq
+    , sorter = Table.unsortable
+    }
+freqs = Dict.fromList([(0,100),(1,200),(2,500),(3,1000)])
+
+freqMap val =
+  option [] [ text (toString val) ]
+viewFreq : Experiment -> Table.HtmlDetails Msg
+viewFreq exp =
+  let 
+    selectEvent =
+      on "change"
+        (Json.Decode.map (applyFilefreq exp.name) targetValueIntParse)
+    freqOptions = 
+      (List.map freqMap (Dict.values freqs))
+  in
+    Table.HtmlDetails []
+                    [select [selectEvent] 
+                    freqOptions
+                    ]
+
+inputLength : Table.Column Experiment Msg
+inputLength =
+  Table.veryCustomColumn
+    { name = "Desired length (s)"
+    , viewData = viewInput
+    , sorter = Table.unsortable
+    }
+applyFilelen: String -> String -> Msg
+applyFilelen filename desiredlen =
+  SelectSubrange (FileLength filename desiredlen)
+
+applyFilefreq: String -> Int -> Msg
+applyFilefreq filename desiredfreq =
+  SelectFreq (FileFreq filename desiredfreq)
+
+viewInput : Experiment -> Table.HtmlDetails Msg
+viewInput exp =
+  Table.HtmlDetails []
+               [ input 
+                  [ type_ "number"
+                  , H.min "0"
+                  , H.max (toString exp.range)
+                  , onInput (applyFilelen exp.name)
+                  ]
+                  []
+               ]
 fetchExperiments: Cmd Msg
 fetchExperiments =
   let
     url =
-      "http://0.0.0.0:5000/experiments"
+      "http://127.0.0.1:5000/experiments"
   in
     Http.send FetchList (Http.get url decodeListExperiments)
 
 decodeExps =
-  Json.Decode.map6 Experiment
+  Json.Decode.map5 RawExperiment
     (field "name" Json.Decode.string)
     (field "excitation" Json.Decode.string)
     (field "damage" Json.Decode.string)
     (field "minseq" Json.Decode.int)
     (field "maxseq" Json.Decode.int)
-    (field "range" Json.Decode.int)
-
 decodeListExperiments =
   Json.Decode.list decodeExps
+
+zip : List a -> List b -> List (a, b)
+zip = List.map2 (,)
