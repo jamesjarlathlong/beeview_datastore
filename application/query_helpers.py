8d5721d3e4a614d51b03802b546b0e36b31653bd
james <jjlong@mit.edu>
2017-06-15 02:29:40 -0400

    added translation to more sane coordinate system

--- application/query_helpers.py
+++ application/query_helpers.py
@@ -1,4 +1,5 @@
 import collections
+from application.translation_dicts import namer_dict, mult_dict
 def withstreamingconn(engine, f):
     def with_connection_(*args, **kwargs):
         # or use a pool, or a factory function...
@@ -31,13 +32,22 @@ def int_to_g(accel):
     volt_to_g = 5/3.3
     num_to_g = num_to_volt*accel*volt_to_g
     return num_to_g
+
 def possible_nodes():
     return [5, 6, 7, 15, 17, 18, 20, 21, 22, 23, 24, 25, 27, 29, 30, 31, 32, 33, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 52, 53, 54, 55, 56, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69]    
 def num_sensors():
     return len(list(itertools.product(possible_nodes(), ['x','y','z'])))
+def new_name(old_name):
+    return namer_dict.get(old_name, old_name)
+def orient_correctly(old_name, value):
+    return mult_dict.get(old_name,1)*value
+def translate_to_consistent(row):
+    return {new_name(k):orient_correctly(k,v) for k,v in row.items()}
 def get_arrs(query):
     translation = {'f1':'seq_id', 'f2':'node', 'f3':'axis','f4':'accel'}
     just_arrays = (just_data(i) for i in query)
     translated = (arraydict_translator(translation, arr) for arr in just_arrays)
     tstamped_arrs = (merge_timestep(onet) for onet in translated)
-    return tstamped_arrs
+    translated_arrs = (translate_to_consistent(arr) for arr in tstamped_arrs)
+    return translated_arrs
+
