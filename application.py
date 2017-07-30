c9abf23f0f240d562f1421b6c5d443160f68d7b7
James <jjlong@mit.edu>
2017-07-30 00:45:00 -0400

    adding node name conversion

--- application.py
+++ application.py
@@ -31,11 +31,13 @@ def index():
 
 @application.route('/experiments', methods = ['GET'])
 def experiments():
-    white_list= ['intact_whitenoise_1', 'ambient_1hr']
+    white_list=['BL_pink_04', 'D3_pink_01', 'BL_pink_05', 'intact_whitenoise_1', 'ambient_1hr', 'D3_pink_02', 'D4_pink_01', 'bolt_loose_white_noise_1', 'bolt_loose_white_noise_2', 'D4_pink_02', 'D5_pink_01', 'D5_pink_02', 'intact_whitenoise_2', 'bolt_loose_white_noise_3', 'D2_pink_01', 'D1_pink_01', 'three_elements_removed_white_noise_1', 'BL_pink_02', 'BL_pink_03']
     all_experiments = [helpers.extract_meta(i) for i in experiment_meta.query.all()]
+    print('exp:',all_experiments)
     white_listed = [i for i in all_experiments if i['name'] in white_list]
-    #all_experiments = [{'name':'experiment1','excitation':'longambientfake','range':10000000,'damage':'undamagedfake','minseq':2,'maxseq':120000000}]
-    return Response(json.dumps(white_listed), status = 200)
+    nicknamed = white_listed#[helpers.nickname(i) for i in white_listed]
+    print(nicknamed)
+    return Response(json.dumps(nicknamed), status = 200)
 
 @application.route('/large.csv', methods = ['POST'])
 def generate_large_csv():
@@ -48,17 +50,17 @@ def generate_large_csv():
     print('formed the query')
     arrs = helpers.get_arrs(query)
     print('got the arrs')
-    noderange = helpers.possible_nodes()
+    noderange = range(1,49)#helpers.possible_nodes()
     writer = get_file_writer(arrs, noderange)
     response = Response(stream_with_context(writer()), mimetype='text/csv')
     #response = Response(status = 200)
-    response.headers["Content-Disposition"] = "attachment; filename=test.csv"
+    response.headers["Content-Disposition"] = "attachment; filename={}_{}Hz.csv".format(specs['folder_name'], specs['freq'])
     return response
 def form_query(specs):
     jsonified = func.array_to_json(func.array_agg(func.row(accel_data.seq_id, accel_data.node, accel_data.axis, accel_data.accel)))
-    starting_seq = int(specs['min_sequence']) + 1000*int(specs['userstart'])
-    ending_seq = int(specs['min_sequence']) + 1000*int(specs['userfinish'])
-    skip_every = int(1000/int(specs['freq']))
+    starting_seq = int(specs['min_sequence']) + 2000*int(specs['userstart'])
+    ending_seq = int(specs['min_sequence']) + 2000*int(specs['userfinish'])
+    skip_every = int(2000/int(specs['freq']))
     print(starting_seq, ending_seq)
     vectors = (db.session.query(accel_data.seq_id, jsonified)
              .group_by(accel_data.seq_id)
@@ -86,4 +88,4 @@ def get_file_writer(arrs, noderange):
             yield val
     return functools.partial(data_writer, header_dict, arrs)
 if __name__ == '__main__':
-    application.run(port=3000)
+    application.run(port=5000)
